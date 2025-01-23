c6dc97d1717a5a00f5e6d489a0b959bfb85c97c8
Go-Jaecheol <gojaech@naver.com>
2025-01-24 05:17:52 +0900

    feat: Reservation 모델 유효성 검사 로직 추가

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,5 +2,8 @@ class Reservation < ApplicationRecord
   belongs_to :user
 
   enum :status, pending: "pending", confirmed: "confirmed", canceled: "canceled", default: "pending"
+
   validates :user_id, :start_time, :end_time, :headcount, presence: true
+  validates :end_time, comparison: { greater_than: :start_time }
+  validates :headcount, numericality: { only_integer: true, greater_than: 0 }
 end
