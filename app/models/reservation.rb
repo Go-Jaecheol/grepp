41464ae8d0f3d2da1896298b989e7e65522fb519
Go-Jaecheol <gojaech@naver.com>
2025-01-24 22:42:13 +0900

    feat: 예약 신청 시 동시간대 최대 인원수 검증하는 로직 구현 (+ 단위 테스트 추가)

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -8,14 +8,27 @@ class Reservation < ApplicationRecord
   validates :headcount, numericality: { only_integer: true, greater_than: 0 }
 
   validate :check_start_time_deadline
+  validate :check_max_headcount
 
   private
 
   DEADLINE = 3.days.from_now
+  MAX_HEADCOUNT = 50_000
 
   def check_start_time_deadline
     if start_time.present? && start_time < DEADLINE
       errors.add(:start_time, "예약은 최소 3일 전까지만 신청할 수 있습니다.")
+    end
+  end
+
+  def check_max_headcount
+    reservations = Reservation.where(
+      "(start_time < ?) AND (end_time > ?) AND status = ?", end_time, start_time, "confirmed"
+    )
+    reserved_count = reservations.sum(:headcount)
+
+    if headcount.present? && reserved_count + headcount > MAX_HEADCOUNT
+      errors.add(:base, "같은 시간대에는 최대 50,000명까지만 예약할 수 있습니다.")
     end
   end
 end
--- test/models/reservation_test.rb
+++ test/models/reservation_test.rb
@@ -6,8 +6,8 @@ class ReservationTest < ActiveSupport::TestCase
     # given & when
     reservation = Reservation.new(
       user: users(:client_1),
-      start_time: Time.now + 4.day,
-      end_time: Time.now + 4.day + 1.hour,
+      start_time: Time.current + 4.day,
+      end_time: Time.current + 4.day + 1.hour,
       headcount: 1
     )
     # then
@@ -17,8 +17,8 @@ class ReservationTest < ActiveSupport::TestCase
   test "user_id가 없으면 예외가 발생한다." do
     # given & when
     reservation = Reservation.new(
-      start_time: Time.now + 4.day,
-      end_time: Time.now + 4.day + 1.hour,
+      start_time: Time.current + 4.day,
+      end_time: Time.current + 4.day + 1.hour,
       headcount: 1
     )
     # then
@@ -29,7 +29,7 @@ class ReservationTest < ActiveSupport::TestCase
     # given & when
     reservation = Reservation.new(
       user: users(:client_1),
-      end_time: Time.now + 4.day + 1.hour,
+      end_time: Time.current + 4.day + 1.hour,
       headcount: 1
     )
     # then
@@ -40,7 +40,7 @@ class ReservationTest < ActiveSupport::TestCase
     # given & when
     reservation = Reservation.new(
       user: users(:client_1),
-      start_time: Time.now + 4.day,
+      start_time: Time.current + 4.day,
       headcount: 1
     )
     # then
@@ -51,8 +51,8 @@ class ReservationTest < ActiveSupport::TestCase
     # given & when
     reservation = Reservation.new(
       user: users(:client_1),
-      start_time: Time.now + 4.day,
-      end_time: Time.now + 4.day + 1.hour
+      start_time: Time.current + 4.day,
+      end_time: Time.current + 4.day + 1.hour
     )
     # then
     assert_not reservation.valid?, "headcount 없이 예약을 생성할 수 없습니다."
@@ -62,8 +62,8 @@ class ReservationTest < ActiveSupport::TestCase
     # given & when
     reservation = Reservation.new(
       user: users(:client_1),
-      start_time: Time.now + 4.day,
-      end_time: Time.now + 4.day - 1.hour,
+      start_time: Time.current + 4.day,
+      end_time: Time.current + 4.day - 1.hour,
       headcount: 1
     )
     # then
@@ -74,8 +74,8 @@ class ReservationTest < ActiveSupport::TestCase
     # given & when
     reservation = Reservation.new(
       user: users(:client_1),
-      start_time: Time.now + 4.day,
-      end_time: Time.now + 4.day + 1.hour,
+      start_time: Time.current + 4.day,
+      end_time: Time.current + 4.day + 1.hour,
       headcount: 0
     )
     # then
@@ -86,8 +86,8 @@ class ReservationTest < ActiveSupport::TestCase
     # given & when
     reservation = Reservation.new(
       user: users(:client_1),
-      start_time: Time.now + 4.day,
-      end_time: Time.now + 4.day + 1.hour,
+      start_time: Time.current + 4.day,
+      end_time: Time.current + 4.day + 1.hour,
       headcount: 1.5
     )
     # then
@@ -98,8 +98,8 @@ class ReservationTest < ActiveSupport::TestCase
     # given & when
     reservation = Reservation.new(
       user: users(:client_1),
-      start_time: Time.now + 2.day,
-      end_time: Time.now + 2.day + 1.hour,
+      start_time: Time.current + 2.day,
+      end_time: Time.current + 2.day + 1.hour,
       headcount: 1
     )
     # then
@@ -110,9 +110,45 @@ class ReservationTest < ActiveSupport::TestCase
     # given & when
     reservation = Reservation.new(
       user: users(:client_1),
-      start_time: Time.now + 3.day,
-      end_time: Time.now + 3.day + 1.hour,
+      start_time: Time.current + 3.day,
+      end_time: Time.current + 3.day + 1.hour,
       headcount: 1
+    )
+    # then
+    assert reservation.valid?, reservation.errors.full_messages
+  end
+
+  test "같은 시간대에 최대 인원 수(50_000명)를 초과하는 예약을 생성하는 경우 예외가 발생한다." do
+    # given & when
+    reservation = Reservation.new(
+      user: users(:client_1),
+      start_time: (Time.current + 4.day).change(hour: 12, min: 0, sec: 0),
+      end_time: (Time.current + 4.day).change(hour: 15, min: 0, sec: 0),
+      headcount: 1_000
+    )
+    # then
+    assert_not reservation.valid?, "같은 시간대에 최대 인원 수를 초과하는 예약을 생성할 수 없습니다."
+  end
+
+  test "예약을 생성해도 같은 시간대에 최대 인원 수(50_000명)를 초과하지 않는 경우 예약이 정상적으로 생성된다." do
+    # given & when
+    reservation = Reservation.new(
+      user: users(:client_1),
+      start_time: (Time.current + 4.day).change(hour: 13, min: 0, sec: 0),
+      end_time: (Time.current + 4.day).change(hour: 14, min: 0, sec: 0),
+      headcount: 1_000
+    )
+    # then
+    assert reservation.valid?, reservation.errors.full_messages
+  end
+
+  test "경곗값인 경우 같은 시간대에 포함하지 않는다 (e.g. [13시~15시], [15시~16시]는 같은 시간대 X" do
+    # given & when
+    reservation = Reservation.new(
+      user: users(:client_1),
+      start_time: (Time.current + 4.day).change(hour: 15, min: 0, sec: 0),
+      end_time: (Time.current + 4.day).change(hour: 16, min: 0, sec: 0),
+      headcount: 1_000
     )
     # then
     assert reservation.valid?, reservation.errors.full_messages
